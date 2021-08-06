37f32b896540d4152ce9565a1e17efb689694bdb
YevaUsatova <yeva.usatova@gmail.com>
2021-08-05 22:42:23 -0400

    commit tree 11baca257881983ddbfc33ce4b38e73ac3064de2

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState}from "react";
 
 function Filter({ onCategoryChange }) {
+const [text, setText]= useState('')
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
-      <select name="filter" onChange={onCategoryChange}>
+      <input type="text" name="search" placeholder="Search..."  />
+      <select name="filter" onChange={onCategoryChange} value={text}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
         <option value="Dairy">Dairy</option>
